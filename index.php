242731d6dfd7bd8fc5c9f7a6a2a786d970ba3c2c
dokokoko <setyohandoko66@gmail.com>
2019-11-15 17:29:29 +0700

    Rubah HTML dan penambahan img profile

--- index.php
+++ index.php
@@ -2,7 +2,10 @@
 <html>
     <head>
         <body>
-            Helloworld
+            <div>
+                <img src="images/234.jpg" alt="profile image" />
+                <h2>Angga Pratama</h2>
+            </div>
         </body>
     </head>
 </html>
